92da41b3fdff83d193cfdebc92b7170fec4ae944
Rosalind W <blackjacksoftwaredev@gmail.com>
2022-01-11 15:14:06 -0600

    Adding cors support

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import logger from 'morgan';
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
 
 const app = express();
 app.use(logger('dev'));
+app.use(
+	cors({
+		origin: 'https://campfire-stream-team-frontend.herokuapp.com'
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
